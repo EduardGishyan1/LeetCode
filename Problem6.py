28dcea3754d19355a3959d8dee0156d2cfc3d026
EduardGishyan <164676522+EduardGishyan1@users.noreply.github.com>
2024-10-16 18:32:37 +0400

    Update Problem6.py

--- Problem6.py
+++ Problem6.py
@@ -1,1 +1,11 @@
+# Time complexity o(n^2)
 
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(len(nums)-1):
+            for j in range(i+1,len(nums)):
+                if nums[i] > nums[j]:
+                    nums[i] , nums[j] = nums[j],nums[i]
