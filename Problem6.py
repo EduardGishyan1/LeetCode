548fb4f16b0df4225c1282efc9dd32d18a8f506b
EduardGishyan <164676522+EduardGishyan1@users.noreply.github.com>
2024-10-19 15:29:29 +0400

    Update Problem6.py

--- Problem6.py
+++ Problem6.py
@@ -9,3 +9,27 @@ class Solution:
             for j in range(i+1,len(nums)):
                 if nums[i] > nums[j]:
                     nums[i] , nums[j] = nums[j],nums[i]
+
+
+# Time complexity o(n)
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        low,mid,high = 0,0,len(nums)-1
+
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[mid],nums[low] = nums[low],nums[mid]
+                mid += 1
+                low += 1
+                
+            elif nums[mid] == 1:
+                mid += 1
+
+            else:
+                nums[mid],nums[high] = nums[high],nums[mid]
+                high -= 1
